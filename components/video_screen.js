1894ed2952235ce57fe44d0f3e319417281bbcc1
Adam Walzer <adamwalzer@gmail.com>
2017-03-30 11:59:02 -0400

    back to video

--- components/video_screen.js
+++ components/video_screen.js
@@ -1,3 +1,6 @@
+const src = 'https://res.cloudinary.com/changemyworldnow/video/upload/v1455037011/' +
+    'Be_Bright_112015_DM_480p_ghb6vh_summbp.mp4';
+
 export default function (props, ref, key) {
     return (
         <skoash.Screen
@@ -5,16 +8,11 @@ export default function (props, ref, key) {
             ref={ref}
             key={key}
             id="video-screen"
-            backgroundAudio={false}
         >
             <skoash.Image className="hidden" src={`${CMWN.MEDIA.FRAME}fr-2.png`} />
             <skoash.Component ref="center" className="center">
                 <skoash.Component ref="frame" className="frame video">
-                    <skoash.GameEmbedder
-                        ref="video"
-                        src="video.html"
-                        pauseOnClick={true}
-                    />
+                    <skoash.Video ref="video" src={src} />
                 </skoash.Component>
             </skoash.Component>
         </skoash.Screen>
